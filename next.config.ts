6a33220be073aac5819a5394382d210aaebb3d00
Trần Hải Đăng <106256609+DangK3@users.noreply.github.com>
2025-11-18 16:49:14 +0700

    Install Supabase, Sonner and update Next.js config
    - Add @supabase/supabase-js and sonner dependencies
    - Configure image qualities and remote patterns in next.config.ts to fix warnings
    - Refactor deployment logic using cross-env

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,34 @@
+// next.config.ts
 import type { NextConfig } from "next";
-const repoName = '/Photographer-Portfolio';
+
+// 1. Kiểm tra xem có đang chạy lệnh deploy không?
+const isGithubPages = process.env.NEXT_PUBLIC_IS_GH_PAGES === 'true';
+
+// 2. Tên repo của bạn (Thay thế đúng tên repo trên GitHub)
+const repoName = '/Photographer-Portfolio'; 
+
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
-    // !! Cảnh báo: Sẽ bỏ qua lỗi build của TypeScript.
-    // Chúng ta dùng cái này TẠM THỜI để vượt qua lỗi cache.
     ignoreBuildErrors: true,
   },
-  output: 'export',
+
+  // TỰ ĐỘNG HÓA:
+  // Nếu là deploy GitHub -> Dùng 'export'. Nếu local -> Dùng mặc định (undefined)
+  output: isGithubPages ? 'export' : undefined,
   
-  // 2. Báo cho Next.js biết đường dẫn gốc
-  // (Rất quan trọng để CSS, JS, và ảnh load đúng)
-  basePath: repoName,
-  
-  // 3. Tắt tối ưu ảnh (bắt buộc cho GitHub Pages)
+  // Nếu là deploy GitHub -> Thêm tên repo. Nếu local -> Không thêm gì cả
+  basePath: isGithubPages ? repoName : undefined,
+
+  // Tắt tối ưu ảnh (GitHub Pages bắt buộc tắt cái này)
   images: {
-    unoptimized: true,
+    unoptimized: true, 
+    qualities: [75, 90],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
   },
 };
 
